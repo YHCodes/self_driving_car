1717d950b47d5dfe7eed21e5c82a673337d0b91c
YHCodes <346095986@qq.com>
2018-05-12 17:26:08 +0800

    remove unused codes

--- computer_vision_and_deep_learning/fundamentals/color_selection.py
+++ computer_vision_and_deep_learning/fundamentals/color_selection.py
@@ -4,18 +4,12 @@ import numpy as np
 
 # Read in the image and print out some stats
 image = mpimg.imread('test.jpg')
-print('This image is: ',type(image), 
-         'with dimensions:', image.shape)
-
-# Grab the x and y size and make a copy of the image
-ysize = image.shape[0]
-xsize = image.shape[1]
+print('This image is: ',type(image), 'with dimensions:', image.shape)
+         
 # Note: always make a copy rather than simply using "="
 color_select = np.copy(image)
 
 # Define our color selection criteria
-# Note: if you run this code, you'll find these are not sensible values!!
-# But you'll get a chance to play with them soon in a quiz
 red_threshold   = 200
 green_threshold = 200
 blue_threshold  = 200
